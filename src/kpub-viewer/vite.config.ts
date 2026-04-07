b7f01d1caff3c1d3e367be1f67e8656ba4480bf3
Adenegar <adenegar9302@gmail.com>
2026-04-07 09:53:29 -1000

    added dev server for testing and ikpub + keck_score columns for ikpub classifications

--- src/kpub-viewer/vite.config.ts
+++ src/kpub-viewer/vite.config.ts
@@ -10,4 +10,10 @@ export default defineConfig({
     chunkSizeWarningLimit: 1000,
     minify: false,
   },
+  server: {
+    proxy: {
+      '/api/kpub': 'http://vm-dev-appserver/api/kpub', // Point to production API
+      // '/api/kpub': 'http://localhost:5001', // Point to dev_server
+    },
+  },
 })

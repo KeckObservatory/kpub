569d5c146f27a2ea6b7f4ecb5c2d893017ec4c7f
Tyler Tucker <tylertucker202@gmail.com>
2026-04-13 10:56:59 -1000

    added for module run

---
+++ src/__main__.py
@@ -0,0 +1,23 @@
+"""
+Entry point for running kpub as a module: python -m kpub
+"""
+import sys
+from .kpub import make_parser, kpub_update, kpub_add, kpub_plot, kpub_plot_data, kpub_delete, kpub_import, kpub_export, kpub_stats, kpub_spreadsheet
+import logging
+
+log = logging.getLogger('KPUB')
+
+if __name__ == "__main__":
+    cmd = sys.argv[1]
+    parser = make_parser()
+    margs = parser.parse_args(sys.argv[1:])
+    if cmd == 'update':      kpub_update(margs.month)
+    elif cmd == 'add':       kpub_add(margs.bibcode, margs.interactive)
+    elif cmd == 'plot':      kpub_plot()
+    elif cmd == 'plot_data': kpub_plot_data(margs.plotname, margs.instruments, margs.year_begin, margs.extrapolate)
+    elif cmd == 'delete':    kpub_delete(margs.bibcode)
+    elif cmd == 'import':    kpub_import(margs.jsonfile)
+    elif cmd == 'export':    kpub_export(margs.monthyear, margs.begin_year, margs.filename, margs.affiliation, margs.csv)
+    elif cmd == 'stats':     kpub_stats()
+    elif cmd == 'spreadsheet': kpub_spreadsheet(margs.filename)
+    else: log.error("Unknown kpub command")

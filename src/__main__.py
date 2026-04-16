41e85ba7f10c9eda883d8a39f53f8eb2802f3a91
Tyler Tucker <tylertucker202@gmail.com>
2026-04-16 09:59:33 -1000

    updating citations for __main__

--- src/__main__.py
+++ src/__main__.py
@@ -5,9 +5,9 @@ import sys
 # Try relative import (when used as installed package)
 # Fall back to absolute import (when imported directly from source)
 try:
-    from .kpub import make_parser, kpub_update, kpub_add, kpub_plot, kpub_plot_data, kpub_delete, kpub_import, kpub_export, kpub_stats, kpub_spreadsheet
+    from .kpub import make_parser, kpub_update, kpub_add, kpub_plot, kpub_plot_data, kpub_delete, kpub_import, kpub_export, kpub_stats, kpub_spreadsheet, kpub_update_citations
 except ImportError:
-    from kpub import make_parser, kpub_update, kpub_add, kpub_plot, kpub_plot_data, kpub_delete, kpub_import, kpub_export, kpub_stats, kpub_spreadsheet
+    from kpub import make_parser, kpub_update, kpub_add, kpub_plot, kpub_plot_data, kpub_delete, kpub_import, kpub_export, kpub_stats, kpub_spreadsheet, kpub_update_citations
 import logging
 
 log = logging.getLogger('KPUB')
@@ -24,5 +24,6 @@ if __name__ == "__main__":
     elif cmd == 'import':    kpub_import(margs.jsonfile)
     elif cmd == 'export':    kpub_export(margs.monthyear, margs.begin_year, margs.filename, margs.affiliation, margs.csv)
     elif cmd == 'stats':     kpub_stats()
+    elif cmd == 'update_citations': kpub_update_citations(margs.year)
     elif cmd == 'spreadsheet': kpub_spreadsheet(margs.filename)
     else: log.error("Unknown kpub command")

44efd322099c73a25eac17deb6804dd0b24be3d2
Tyler Tucker <tylertucker202@gmail.com>
2026-04-17 08:41:41 -1000

    sorry, was told to revert

--- src/kpub.py
+++ src/kpub.py
@@ -122,7 +122,7 @@ class PublicationDB(MongoDBConnector):
         
         acknowledgement = self.config.get('acknowledgement', [])
         keys = snippits.keys()
-        affiliation = 'unrelated' # default
+        affiliation = 'unknown' # default
         hasAcknowledgement = False
         reason = "Neither instr nor ack found."
         if any(x in keys for x in acknowledgement):
